1902f7e09814bd2881b2b5a70e1a8c7076f9bace
Jannatun Nahar Papia <papiamcode@Jannatuns-MacBook-Pro.local>
2021-12-11 03:13:23 +0600

    learning swift

---
+++ playground/Shared/playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  playgroundApp.swift
+//  Shared
+//
+//  Created by Jannatun Nahar Papia  on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
