cc293743962e5cb305fe24549ad51e562dd5c2bc
Jannatun Nahar Papia <papiamcode@Jannatuns-MacBook-Pro.local>
2021-12-15 01:57:57 +0600

    Simple and Easy Math Game

---
+++ Math Game/Math Game/Math_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_GameApp.swift
+//  Math Game
+//
+//  Created by Jannatun Nahar Papia  on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Math_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
