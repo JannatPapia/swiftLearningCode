5bf6a88d1255cee3e5ff30be8b1809d96b31af46
Jannatun Nahar Papia <papiamcode@Jannatuns-MacBook-Pro.local>
2021-12-17 12:04:42 +0600

    WeatherView UI

---
+++ WeatherApp/WeatherApp/Extentions.swift
@@ -0,0 +1,18 @@
+//
+//  Extentions.swift
+//  WeatherApp
+//
+//  Created by Jannatun Nahar Papia  on 17/12/21.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format: "%.0f", self) // round the function 0 decimal
+    }
+}
+
+
+
+
--- WeatherApp/WeatherApp/Views/WeatherView.swift
+++ WeatherApp/WeatherApp/Views/WeatherView.swift
@@ -10,7 +10,37 @@ import SwiftUI
 struct WeatherView: View {
     var weather: ResponseBody
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading){
+            VStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weather.name)
+                        .bold().font(.title)
+                    
+                    Text("Today,\(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                }
+            }
+            //.padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer()
+            
+            VStack {
+                HStack {
+                    Text(weather.main.feelsLike)
+                   // Text(weather.main.feelsLike)
+                }
+                
+            }
+            .frame(maxWidth: .infinity)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        
+            .edgesIgnoringSafeArea(.bottom)
+            .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+            .preferredColorScheme(.dark)
+        //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
