67309e99d2b5c092f0e44c903b4477237f670b0b
Jannatun Nahar Papia <papiamcode@Jannatuns-MacBook-Pro.local>
2021-12-17 16:56:21 +0600

    weather app done

--- WeatherApp/WeatherApp/Views/WeatherView.swift
+++ WeatherApp/WeatherApp/Views/WeatherView.swift
@@ -9,16 +9,21 @@ import SwiftUI
 
 struct WeatherView: View {
     var weather: ResponseBody
+    
     var body: some View {
+        
         ZStack(alignment: .leading){
+            
             VStack {
+                
                 VStack(alignment: .leading, spacing: 5) {
                     Text(weather.name)
-                        .bold().font(.title)
+                        .bold()
+                        .font(.title)
                     
-                    Text("Today,\(Date().formatted(.dateTime.month().day().hour().minute()))")
+                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
                         .fontWeight(.light)
-                }
+                
             }
             //.padding()
             .frame(maxWidth: .infinity, alignment: .leading)
@@ -27,23 +32,81 @@ struct WeatherView: View {
             
             VStack {
                 HStack {
-                    Text(weather.main.feelsLike)
+                    VStack(spacing: 20){
+                        //display symble
+                        Image(systemName: "sun.max.fill")
+                            .font(.system(size: 40))
+                        //display weather condition
+                        Text(weather.weather[0].main)//get the first of array
+                    }
+                    .frame(width: 150, alignment: .leading)
+                    
+                    Spacer()
+                    
+                    Text(weather.main.feelsLike.roundDouble() + "°")
+                        .font(.system(size: 100))
+                        .fontWeight(.bold)
+                        .padding()
                    // Text(weather.main.feelsLike)
+                       // .roundDouble() + "^"
                 }
                 
+                Spacer()
+                    .frame(height: 80)
+                
+                AsyncImage(url: URL(string: "SodaPDF-converted-pedro-lastra-Nyvq2juw4_o-unsplash.png"))
+                {
+                    image in image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 350)
+                }placeholder: {
+                    ProgressView()
+                }
+                Spacer()
+            
             }
-            .frame(maxWidth: .infinity)
+            .frame(maxWidth: .infinity, alignment: .trailing)
         }
         .padding()
         .frame(maxWidth: .infinity, alignment: .leading)
-        
+             
+            VStack {
+                Spacer()
+                VStack(alignment: .leading, spacing: 20){
+                    Text("Weather Now")
+                        .bold().padding(.bottom)
+                    
+                    HStack {
+                        WeatherRow(logo: "thermometer", name: "Min temp", value: (weather.main.tempMin.roundDouble() + "°"))
+                        
+                        Spacer()
+                        WeatherRow(logo: "thermometer", name: "Max temp", value: (weather.main.tempMax.roundDouble() + "°"))
+                    }
+                    
+                    HStack {
+                        WeatherRow(logo: "wind", name: "Wind Speed", value: (weather.wind.speed.roundDouble() + "m/s"))
+                        
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Humidity", value: (weather.main.humidity.roundDouble() + "%"))
+                    }
+                    
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                    .padding(.bottom, 20)
+                    .foregroundColor(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+                    .background(.white)
+                    .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
+            
+            
             .edgesIgnoringSafeArea(.bottom)
             .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
             .preferredColorScheme(.dark)
-        //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+       
+        }
 }
-
+}
 struct WeatherView_Previews: PreviewProvider {
     static var previews: some View {
         WeatherView(weather: previewWeather)
